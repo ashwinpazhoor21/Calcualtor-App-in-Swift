2f7b08cf8ba8da484d35098b0f3578fe9fc86e6c
Ashwin Pazhoor <ashwinpazhoor@gmail.com>
2024-08-29 13:52:58 -0700

    Inital Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ashwin Pazhoor on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
